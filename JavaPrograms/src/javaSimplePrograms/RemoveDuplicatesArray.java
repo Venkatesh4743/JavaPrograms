63f5cdee14314031b6a7c442bcb1750ef427be54
Venkatesh4743 <yrvenkatesh6179@gmail.com>
2024-05-29 20:41:47 +0530

    commit

--- JavaPrograms/src/javaSimplePrograms/RemoveDuplicatesArray.java
+++ JavaPrograms/src/javaSimplePrograms/RemoveDuplicatesArray.java
@@ -7,17 +7,22 @@ import java.util.Set;
 public class RemoveDuplicatesArray {
 
 	public static void main(String[] args) {
-		
-		int[]a= {10,5,5,8,7,8,5};
-		Set<Integer> st=new HashSet<Integer>();
-		
-		for(int i=0;i<a.length;i++) {
-			
-			st.add(a[i]);
-		}
-		Integer[] b = st.toArray(new Integer[st.size()]);
-		
-		System.out.println(Arrays.toString(b));
+	    
+	    // Initialize an array with given values
+	    int[] a = {10, 5, 5, 8, 7, 8, 5};
+	    
+	    // Create a HashSet to store unique elements
+	    Set<Integer> st = new HashSet<Integer>();
+	    
+	    // Add each element of the array to the HashSet
+	    for (int i = 0; i < a.length; i++) {
+	        st.add(a[i]);
+	    }
+	    
+	    // Convert the HashSet to an array of Integers
+	    Integer[] b = st.toArray(new Integer[st.size()]);
+	    
+	    // Print the array of unique elements
+	    System.out.println(Arrays.toString(b));
 	}
-
 }
