ee2eb8354d7ad787a32ba0cae43f3b2c6cee0ca3
Venkatesh4743 <yrvenkatesh6179@gmail.com>
2024-05-29 19:28:03 +0530

    commit

---
+++ JavaPrograms/src/javaSimplePrograms/CountNumberOfDigit.java
@@ -0,0 +1,26 @@
+package javaSimplePrograms;
+
+import java.util.Scanner;
+
+public class CountNumberOfDigit {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter numbers");
+		int num = sc.nextInt();
+		
+		int digitCount = 0;
+		
+		while(num>0) {
+			
+			digitCount++;
+			
+			num= num/10;
+			
+			System.out.println(digitCount);
+			
+		}
+	}
+
+}
